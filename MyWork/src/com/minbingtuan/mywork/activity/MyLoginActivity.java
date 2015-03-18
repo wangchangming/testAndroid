fd85cab83aae2dcba1bb028402cdc16f81c7472d
wangchangming <821649975@qq.com>
2015-03-18 09:48:49 +0800

    接手代码2015-03-18

---
+++ MyWork/src/com/minbingtuan/mywork/activity/MyLoginActivity.java
@@ -0,0 +1,164 @@
+package com.minbingtuan.mywork.activity;
+
+import java.util.HashMap;
+import org.json.JSONObject;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.minbingtuan.mywork.MyApplication;
+import com.minbingtuan.mywork.R;
+import com.minbingtuan.mywork.utils.StringUtils;
+import com.minbingtuan.mywork.utils.VolleyErrorHelper;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MyLoginActivity extends Activity {
+
+	private Button buttonLogin = null;
+	private Button buttonRegister = null;
+	private EditText mEditTextUserName = null;
+	private EditText mEditTextUserPassWord = null;
+	private String mUserName;
+	private String mPassWord;
+
+	private MyApplication myApp;
+
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_login);
+		myApp = (MyApplication) getApplication();
+
+		if (MyApplication.getLoginStatus()) {
+			Intent intent = new Intent();
+			intent.setClass(MyLoginActivity.this, MyWorkActivity.class);
+			startActivity(intent);
+			finish();
+		}
+
+		mEditTextUserName = (EditText) findViewById(R.id.EditTextUserName);
+		mEditTextUserPassWord = (EditText) findViewById(R.id.EditTextUserPassWord);
+		buttonLogin = (Button) findViewById(R.id.buttonLogin);
+		buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+		buttonLogin.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				mUserName = mEditTextUserName.getText().toString();
+				mPassWord = mEditTextUserPassWord.getText().toString();
+				if (TextUtils.isEmpty(mUserName) || TextUtils.isEmpty(mPassWord)) {
+					Toast.makeText(getApplicationContext(), getString(R.string.Please_enter_the_UserName_to_log_on),
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+				if (Utils.isFastDoubleClick()) {  
+			        return;  
+			    }
+				if (!myApp.isConnect()) {
+					Toast.makeText(getApplicationContext(), getString(R.string.NetError), Toast.LENGTH_SHORT).show();
+					return;
+				}
+				HttpGetRequestLogin(mUserName, mPassWord);
+			}
+		});
+
+		buttonRegister = (Button) findViewById(R.id.buttonRegister);
+		buttonRegister.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(MyLoginActivity.this, MyRegisterActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+	}
+
+	public void HttpGetRequestLogin(String mUserName, String mPassWord) {
+		RequestQueue queue = Volley.newRequestQueue(this);
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("userName", mUserName);
+		params.put("userPwd", mPassWord);
+		String url = MyApplication.LocalLoginUrl;
+		url += StringUtils.encodeUrl(params);
+
+		JsonObjectRequest jsObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+				new Response.Listener<JSONObject>() {
+
+					@Override
+					public void onResponse(JSONObject response) {
+						int status = response.optInt("status");
+						if (status == 0) {
+							JSONObject jobj = response.optJSONObject("userInfo");
+							int id = jobj.optInt("id");
+							int groupId = jobj.optInt("groupId");
+							String userName = jobj.optString("userName");
+							String realName = jobj.optString("realName");
+							String pwd = jobj.optString("userPwd");
+							String mobile = jobj.optString("mobile");
+							String email = jobj.optString("email");
+							String birthday = jobj.optString("birthday");
+							String groupName = jobj.optString("groupName");
+							MyApplication myApp = (MyApplication) getApplication();
+							myApp.setUserInfo(id, userName, realName, mobile, email, groupId, pwd, birthday,groupName);
+							myApp.startGPSService();
+							myApp.setLoginStatus(true);
+
+							Toast.makeText(getApplicationContext(), getString(R.string.Login_successful),
+									Toast.LENGTH_SHORT).show();
+							Intent intent = new Intent();
+							intent.setClass(MyLoginActivity.this, MyWorkActivity.class);
+							startActivity(intent);
+							finish();
+						}
+						if(status == -1){
+							Toast.makeText(getApplicationContext(), getString(R.string.PassWordError),
+									Toast.LENGTH_SHORT).show();
+						}
+						if (status == -2) {
+							Toast.makeText(getApplicationContext(), getString(R.string.The_user_is_not_registered),
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+
+				}, new Response.ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						Toast.makeText(getApplicationContext(),
+								VolleyErrorHelper.handleServerError(error, getApplication()), Toast.LENGTH_SHORT)
+								.show();
+					}
+
+				});
+		queue.add(jsObjectRequest);
+	}
+	static class Utils {  
+	    public static long lastClickTime;  
+	    public static boolean isFastDoubleClick() {  
+	        long time = System.currentTimeMillis();     
+	        if ( time - lastClickTime < 20000) {     
+	            return true;     
+	        }     
+	        lastClickTime = time;     
+	        return false;     
+	    }  
+	}  
+}
---
+++ MyWork/src/com/minbingtuan/mywork/utils/Tools.java
@@ -0,0 +1,22 @@
+package com.minbingtuan.mywork.utils;
+
+import android.os.Environment;
+/**
+ * 
+ * 
+ */
+public class Tools {
+	/**
+	 * 检查是否存在SDCard
+	 * @return
+	 */
+	public static boolean hasSdcard(){
+		String state = Environment.getExternalStorageState();
+		if(state.equals(Environment.MEDIA_MOUNTED)){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+}
