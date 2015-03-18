3c2e57e4c2ba9b2208bcba98f247f1603dbded6a
wangchangming <821649975@qq.com>
2015-03-18 09:45:33 +0800

    测试2015-03-18

--- MyWork/gen/com/minbingtuan/mywork/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.minbingtuan.mywork;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- MyWork/src/com/minbingtuan/mywork/model/MySearchPosition.java
+++
@@ -1,7 +0,0 @@
-package com.minbingtuan.mywork.model;
-
-public class MySearchPosition {
-	public int mType;
-	public String mDateTime;
-	public String mPosition;
-}
